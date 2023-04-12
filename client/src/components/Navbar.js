5e31765143097816465d03373463d1a39a7b5dd1
Prasanna <prasannaacharya2073@gmail.com>
2023-04-12 13:53:24 +0545

    Navbar added

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Paper } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <>
+      <Paper
+        sx={{ float: "top", width: "100vw", height: "60px" }}
+        elevation={3}
+      >
+        Navbar
+      </Paper>
+    </>
+  );
+};
+
+export default Navbar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <h1>Home Page</h1>
+    </div>
+  );
 };
 
 export default Home;
