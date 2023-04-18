091e1a834a9aa163c90335c1cb248c734ae91ad8
Prasanna <prasannaacharya2073@gmail.com>
2023-04-18 12:45:35 +0545

    Preserving user data on page refresh function added successfully

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
-
+import { useDispatch } from "react-redux";
 import TopNav from "../components/TopNav";
+import { login } from "../store/slices/authSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(login(JSON.parse(localStorage.getItem("user"))));
+  }, [dispatch]);
+
   return (
     <div>
       <Navbar />
