3cedebfde7dc7a9bd8ab48322db7e46a7f1a7374
Prasanna <prasannaacharya2073@gmail.com>
2023-04-18 14:21:16 +0545

    Login data preservation done in other pages too on refresh but not fully functional

--- client/src/pages/MaterialsPage.js
+++ client/src/pages/MaterialsPage.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TopNav from "../components/TopNav";
 import Navbar from "../components/Navbar";
+import { useDispatch } from "react-redux";
+import { login } from "../store/slices/authSlice";
 
 const MaterialsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(login(JSON.parse(localStorage.getItem("user"))));
+  }, [dispatch]);
+
   return (
     <div>
       <Navbar />
